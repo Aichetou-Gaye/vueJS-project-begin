ddb8f1e63a1dc09579168098007fe7c63953cf02
aichetou1408 <aichetouahmedgaye@gmail.com>
2024-08-06 14:49:46 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <HelloWorld 
+      @update-name="name => display(name)"
+    />
+
+    <!-- <span> {{ sentence }}</span>
+    <br>
+    <br>
+
+    <input type="text" v-model="name"><br><br>
+    <input type="number" v-model="age"> -->
+  </div>
+
+</template>
+
+<script setup>
+import HelloWorld from './components/HelloWorld.vue';
+
+const display = (name) => {
+  console.log('App.vue : ' + name)
+}
+
+// import { watch, computed, ref } from 'vue';
+
+// const sentence = computed(() => {
+//   return `Bonjour, je m'appelle ${name.value} et j'ai ${age.value} ans.`
+// })
+// const name = ref('Bideew')
+// const age = ref(24)
+
+// watch (name, (newValue, oldValue) => {
+//   console.log(newValue, oldValue)
+// })
+
+
+// const user = reactive({
+//   name: "Bideew ",
+//   age: 24,
+// })
+// const display = () => {
+//   console.log(user.name, user.age)
+// }
+// display()
+
+</script>
+
+<style scoped>
+.bold {
+  font-weight: bolder;
+}
+
+.text-red {
+  color: indianred;
+  font-size: 2em;
+}
+
+.text-green {
+  color: green;
+  font-size: 2em;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1>Hello world!</h1>
+
+        {{ name }}
+
+        <input type="text" v-model="name" @input="handleUpdate">
+    </div>
+
+</template>
+
+<script setup>
+
+import { ref } from 'vue';
+
+const emit = defineEmits(['updateName'])
+
+const handleUpdate = (event) => {
+    emit('updateName', event.target.value)
+}
+const name = ref('')
+
+
+</script>
